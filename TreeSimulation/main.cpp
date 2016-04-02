4fc117cf0757908dc28a4b2899e72ca77bf9b2e4
Penspinner <stevenliao@stevens-macbook-pro.local>
2016-04-02 13:35:26 -0400

    Initial Commit

---
+++ TreeSimulation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TreeSimulation
+//
+//  Created by Steven Liao on 4/2/16.
+//  Copyright © 2016 Steven Liao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
